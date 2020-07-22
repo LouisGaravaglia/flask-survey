3e1aaa2eda0bedf194ace919fed59dafcd240d8a
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-07-22 10:57:50 -0700

    everything works, going to use sessions intead of pushing to array

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "4534gdghjk5d#$RGR^HDG"
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False 
 
 debug = DebugToolbarExtension(app)
 
@@ -30,15 +31,19 @@ def question_0():
     main_question = satisfaction_survey.questions[0]
     question = main_question.question
     choices = main_question.choices
+    text = main_question.allow_text
     
-    return render_template("question_0.html", question=question, choices=choices)
+    return render_template("question_0.html", question=question, choices=choices, text=text)
 
 
 @app.route('/questions/0/answer', methods=["POST"])
 def answer_0():
     """keeps track of answer to first question"""
     option = request.form["options"]
+    text = request.form.get("text", "false")
     answers.append(option)
+    if not text == "false":
+        answers.append(text)
     
     return redirect("/questions/1")    
 
@@ -50,17 +55,81 @@ def question_1():
     main_question = satisfaction_survey.questions[1]
     question = main_question.question
     choices = main_question.choices
-    
-    return render_template("question_1.html", question=question, choices=choices)
+    text = main_question.allow_text
+
+
+    return render_template("question_1.html", question=question, choices=choices, text=text)
     
 @app.route('/questions/1/answer', methods=["POST"])
 def answer_1():
     """keeps track of answer to second question"""
     option = request.form["options"]
+    text = request.form.get("text", "false")
     answers.append(option)
+    if not text == "false":
+        answers.append(text)
     
-    # return redirect("/")    
-    return render_template("answer_1.html", answers=answers)
+    return redirect("/questions/2")    
+    
 
 
+# /////////////////////////////////////////////////////// THIRD QUESTION
+
+@app.route('/questions/2')
+def question_2():
+    """shows third question"""
+    main_question = satisfaction_survey.questions[2]
+    question = main_question.question
+    choices = main_question.choices
+    text = main_question.allow_text
+
+
+    return render_template("question_2.html", question=question, choices=choices, text=text)
+    
+@app.route('/questions/2/answer', methods=["POST"])
+def answer_2():
+    """keeps track of answer to third question"""
+    option = request.form["options"]
+    text = request.form.get("text", "false")
+    answers.append(option)
+    if not text == "false":
+        answers.append(text)
+    
+    return redirect("/questions/3")    
+
+# /////////////////////////////////////////////////////// FOURTH QUESTION
+
+@app.route('/questions/3')
+def question_3():
+    """shows fourth question"""
+    main_question = satisfaction_survey.questions[3]
+    question = main_question.question
+    choices = main_question.choices
+    text = main_question.allow_text
+
+
+    return render_template("question_3.html", question=question, choices=choices, text=text)
+    
+@app.route('/questions/3/answer', methods=["POST"])
+def answer_3():
+    """keeps track of answer to third question"""
+    option = request.form["options"]
+    text = request.form.get("text", "false")
+    answers.append(option)
+    if not text == "false":
+        answers.append(text)
+    
+    return redirect("/thank_you")
+
+# /////////////////////////////////////////////////////// THANK YOU
+
+@app.route('/thank_you')
+def thanks():
+    """thanks the user for subming survey"""
+
+
+    return render_template("thank_you.html", answers=answers)
+    
+
+    
 
