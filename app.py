dc2c9f52e90d87379c309a2927573a1b5a82fe74
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-07-21 20:34:01 -0700

    passing radio button value to add to session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, session, make_response
+from flask import Flask, request, render_template, session, make_response, redirect
 from surveys import satisfaction_survey
 from random import choice, randint
 
@@ -16,3 +16,29 @@ def home_page():
     instructions = satisfaction_survey.instructions
 
     return render_template("home.html", title=title, instructions=instructions)
+
+
+
+@app.route('/questions/0')
+def question_0():
+    """shows first question"""
+    main_question = satisfaction_survey.questions[0]
+    question = main_question.question
+    choices = main_question.choices
+    
+
+    return render_template("question_0.html", question=question, choices=choices)
+
+
+@app.route('/questions/0/answer')
+def answer_0():
+    """keeps track of answer to first question"""
+    option = request.args.get("options")
+    session[Question_0] = option
+    
+    
+
+    return redirect("/")    
+    # return render_template("answer_0.html", option=option)
+
+
